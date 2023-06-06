e58fb5c0ba999ad5f31d4b627db4deb79ba2af25
astha05 <asthash77@gmail.com>
2023-06-06 08:19:47 +0530

    Updated code with new features

--- src/Components/Bootstrap/Form.js
+++ src/Components/Bootstrap/Form.js
@@ -51,7 +51,7 @@ function Form() {
 <table class="table">
 <thead>
   <tr>
-    <th scope="col">#</th>
+    <th scope="col">id</th>
     <th scope="col">First</th>
     <th scope="col">Last</th>
     <th scope="col">Handle</th>
